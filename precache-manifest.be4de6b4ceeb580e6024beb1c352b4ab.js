7a642d1d7d55840a8f7d9acc121597db5adc5758
Jennifer Browning <browningjen@yahoo.com>
2018-11-27 14:28:17 -0500

    Updates

---
+++ precache-manifest.be4de6b4ceeb580e6024beb1c352b4ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e5813c926338b9347bf0",
+    "url": "/github-card/static/js/runtime~main.e5813c92.js"
+  },
+  {
+    "revision": "0b3e8b71219b4ca79a55",
+    "url": "/github-card/static/js/main.0b3e8b71.chunk.js"
+  },
+  {
+    "revision": "6c1051e0898a6a87e718",
+    "url": "/github-card/static/js/1.6c1051e0.chunk.js"
+  },
+  {
+    "revision": "0b3e8b71219b4ca79a55",
+    "url": "/github-card/static/css/main.04adb4b4.chunk.css"
+  },
+  {
+    "revision": "5fddfec3fe5d1cb7fd5c46b5426ddbf5",
+    "url": "/github-card/index.html"
+  }
+];
